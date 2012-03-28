3f2351d10cd65349121be2015982d01bab3502ae
Stefan Moser <stefanm@iqmetrix.com>
2012-03-28 08:57:45 -0700

    added some very basic serialization examples

---
+++ src/serialization/Serializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace serialization
+{
+    public class Serializer
+    {
+        public string Serialize(object source)
+        {
+            string serializedString = "";
+
+            Type serializingType = source.GetType();
+            serializedString += serializingType.Name;
+
+            var properties = serializingType.GetProperties();
+            foreach (var property in properties)
+            {
+                var propertyName = property.Name;
+                var propertyValue = property.GetGetMethod().Invoke(source, null);
+
+                serializedString += Environment.NewLine;
+                serializedString += "\t";
+                serializedString += propertyName;
+                serializedString += ":";
+                serializedString += " ";
+                serializedString += propertyValue.ToString();
+            }
+
+            return serializedString;
+        }
+    }
+}
---
+++ src/tests/SampleTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace serialization.tests
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        [Test]
+        public void serializes_the_class_name()
+        {
+            var someClass = new SomeClass();
+            var serializer = new Serializer();
+
+            var serializedString = serializer.Serialize(someClass);
+
+            serializedString.Should().Be("SomeClass");
+        }
+
+        public class SomeClass
+        {
+        }
+
+        [Test]
+        public void serializes_public_getter_property()
+        {
+            var someClass = new ClassWithPublicAutoProperty() {SomeAutoProperty = "Foo"};
+            var serializer = new Serializer();
+
+            var serializedString = serializer.Serialize(someClass);
+
+            serializedString.Should().Be(string.Format("ClassWithPublicAutoProperty{0}{1}SomeAutoProperty:{2}Foo", Environment.NewLine, "\t", " "));
+        }
+
+        public class ClassWithPublicAutoProperty
+        {
+            public string SomeAutoProperty { get; set; }
+        }
+    }
+}
